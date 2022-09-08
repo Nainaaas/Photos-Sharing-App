dc11d978f441bdbd214ab181ac62e19a30d1ab4b
Nainaaas <shahina.sikkander@gmail.com>
2022-09-08 10:29:20 +0400

    first commit

---
+++ Photos Sharing App/StringValues.swift
@@ -0,0 +1,15 @@
+//
+//  StringValues.swift
+//  Photos Sharing App
+//
+//  Created by shahina kassim on 08/09/2022.
+//
+
+import Foundation
+struct StringValues{
+    
+    static let imageShare = "Image sharing"
+    static let url = "sdnjsdksdnsd"
+    static let alertMsg = "Please pic any images"
+    static let alertOK = "OK"
+}
--- Photos Sharing App/ViewController.swift
+++ Photos Sharing App/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    @IBOutlet weak var pickButton: UIButton!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.scrollView.minimumZoomScale = 1.0
+        self.scrollView.maximumZoomScale = 6.0
+    
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pickButtonAction(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            self.imagePicker.delegate = self
+            imagePicker.isEditing = false
+            imagePicker.sourceType = .savedPhotosAlbum
+            
+            self.present(imagePicker, animated: true)
+            
+        }
+        
+    }
+    @IBAction func shareAction(_ sender: Any) {
+        if let image = imageView.image{
+            let items : [Any] = [StringValues.imageShare, imageView.image!]
+            let activityVC = UIActivityViewController(activityItems: items, applicationActivities: nil)
+            self.present(activityVC, animated: true)
+        }
+        else{
+            let alert = UIAlertController(title: StringValues.alertMsg, message: nil, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: StringValues.alertOK, style: .default) { UIAlertAction in
+                
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+            
+        }
+       
+    }
+    
 }
-
+extension ViewController: UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.dismiss(animated:true) { [self] in
+            self.imageView.image = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage)
+        }
+       
+    
+    }
+    
+    
+}
+extension ViewController: UIScrollViewDelegate{
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+    }
+}
